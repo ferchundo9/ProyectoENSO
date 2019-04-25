120cb17da2a552513a6c245d4d4cc7f7ba0861ee
Fer <ferchundo9@gmail.com>
2019-04-25 17:04:41 +0200

    asdfgh

--- src/analisis/Analizador.java
+++ src/analisis/Analizador.java
@@ -12,61 +12,62 @@ import com.opencsv.CSVWriter;
 import sensorizacion.DatosVitales;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class Analizador implements InterfazAlarmas,InterfazDatosInstantaneos,InterfazEstadisticas{
+public class Analizador implements InterfazAlarmas, InterfazDatosInstantaneos, InterfazEstadisticas {
 	private HashMap<String, Alarma> alarmas;
-	private HashMap<String,Estadisticas> estadisticas;	// !FIXME PLEASE!!
-	
+	private HashMap<String, Estadisticas> estadisticas; // !FIXME PLEASE!!
+	private HashMap<String, ArrayList<DatosVitales>> datos;
+
 	private Analizador() {
-		this.alarmas =new HashMap<>();
-		this.estadisticas=new HashMap<>();
+		this.alarmas = new HashMap<>();
+		this.estadisticas = new HashMap<>();
+		this.datos = new HashMap<>();
 	}
-	
+
 	public void GuardarEstadisticas() {
 		String archCSV = "";
-		ArrayList<String[]> documento=new ArrayList<>();
+		ArrayList<String[]> documento = new ArrayList<>();
 		String[] fila;
-		Set<String> ids=estadisticas.keySet();
-		for(String id:ids) {
-			fila= new String[8];
-			fila[0]=id;
-			fila[1]=estadisticas.get(id).getMaximo();
-			fila[2]=estadisticas.get(id).getMinimo();
-			fila[3]=estadisticas.get(id).getMedia();
-			fila[4]=estadisticas.get(id).getAlarmasFC();
-			fila[5]=estadisticas.get(id).getAlarmasTem();
-			fila[6]=estadisticas.get(id).getTensionAlarmaFC();
-			fila[7]=estadisticas.get(id).getTensionAlarmaTem();
+		Set<String> ids = estadisticas.keySet();
+		for (String id : ids) {
+			fila = new String[8];
+			fila[0] = id;
+			fila[1] = estadisticas.get(id).getMaximo();
+			fila[2] = estadisticas.get(id).getMinimo();
+			fila[3] = estadisticas.get(id).getMedia();
+			fila[4] = estadisticas.get(id).getAlarmasFC();
+			fila[5] = estadisticas.get(id).getAlarmasTem();
+			fila[6] = estadisticas.get(id).getTensionAlarmaFC();
+			fila[7] = estadisticas.get(id).getTensionAlarmaTem();
 			documento.add(fila);
 		}
 		try {
-			CSVWriter writer = new CSVWriter(new FileWriter(archCSV),';');
+			CSVWriter writer = new CSVWriter(new FileWriter(archCSV), ';');
 			writer.writeAll(documento);
 		} catch (IOException e) {
 			System.out.println(e);
 		}
-		
-		
-		for(int i = 10; i > 0; i--) {
-			while(i != i) {
+
+		for (int i = 10; i > 0; i--) {
+			while (i != i) {
 				// woah
 			}
 		}
-		
-		
+
 	}
-	
+
 	public void CrearAlarma(String name) {
-		Alarma alarma=new Alarma(name);
-		alarmas.put(alarma.getNombre(),alarma);
+		Alarma alarma = new Alarma(name);
+		alarmas.put(alarma.getNombre(), alarma);
 	}
-	
+
 	public void EliminarAlarma(String name) {
 		alarmas.remove(name);
 	}
+
 	public void generarEstadisticas() {
 		throw new NotImplementedException();////////////////////////////////////////
 	}
-	
+
 	public void recuperarEstadisticas() {
 		throw new NotImplementedException();//////////////////////////////////////////
 	}
@@ -77,15 +78,22 @@ public class Analizador implements InterfazAlarmas,InterfazDatosInstantaneos,Int
 	}
 
 	@Override
-	public void EnviarDatosInstantaneos(HashMap<String, DatosVitales> datos) {
-		throw new NotImplementedException();///////////////////////////////
+	public void EnviarDatosInstantaneos(HashMap<String, DatosVitales> datos2) {
+		Set<String> keys=datos2.keySet();
+		for(String key:keys) {
+			if(datos.containsKey(key)) {
+				datos.get(key).add(datos2.get(key));
+			}
+			else {
+				
+			}
+		}
 	}
 
 	@Override
-	public HashMap<String,Estadisticas> recibirEstadisticas() {
+	public HashMap<String, Estadisticas> recibirEstadisticas() {
 		// TODO Auto-generated method stub
 		return new HashMap<>(estadisticas);
 	}
 
-	
 }
