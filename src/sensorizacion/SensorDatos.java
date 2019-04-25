81d3bd079db2a383a4f089e4c40fc0a121c7b665
lazaretto <gonza99nf@gmail.com>
2019-04-25 17:01:28 +0200

    CrearDatos() terminado

--- src/sensorizacion/SensorDatos.java
+++ src/sensorizacion/SensorDatos.java
@@ -2,28 +2,34 @@ package sensorizacion;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import almacenDatos.ImplementacionAlmacen;
-import almacenDatos.InterfazDatosSimulados;
 import gestionPacientes.Paciente;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class SensorDatos{
+public class SensorDatos {
 	private HashMap<String, DatosVitales> mapa;
 	private ArrayList<Paciente> pacientes;
 	private ImplementacionAlmacen almacen;
-	
+
 	public SensorDatos(ArrayList<Paciente> pacientes) {
-		this.pacientes=pacientes;
-		mapa=new HashMap<String/*id*/, DatosVitales>();
+		this.pacientes = pacientes;
+		mapa = new HashMap<String/* id */, DatosVitales>();
 	}
-	
+
 	public void CrearDatos() {
-		throw new NotImplementedException();
+		for(int j = 0; j < 100000; j++) {
+			double fc;
+			double temp;
+			for (int i = 0; i < pacientes.size(); i++) {
+				fc = (Math.random() * ((230) + 1));
+				temp = (Math.random() * ((45 - 30) + 1)) + 30;
+				mapa.put(Integer.toString(pacientes.get(i).getId()), new DatosVitales(fc, temp));
+			}
+
+			EnviarDatos();
+		}
 	}
-	
+
 	public void EnviarDatos() {
 		almacen.EnviarDatos(mapa);
 	}
